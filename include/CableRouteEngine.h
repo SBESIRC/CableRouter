46d98e3c995ee2990a9ea4db020d17294537a9b6
THAPE\wushihang <wushihang@thape.com.cn>
2021-09-07 14:16:42 +0800

    Revert "export class"
    This reverts commit 87c34b464fd8b4da78ef0b170d12af3a5868ed23.

--- include/CableRouteEngine.h
+++ include/CableRouteEngine.h
@@ -1,11 +1,5 @@
 #include <string>
 
-#ifdef THMODULE_DLL
-#  define THMODULE_EXPORT __declspec(dllexport)
-#else
-#  define THMODULE_EXPORT __declspec(dllimport)
-#endif
-
 #ifndef _CABLEROUTER_INTERFACE_H_
 #define _CABLEROUTER_INTERFACE_H_
 
@@ -16,7 +10,7 @@ namespace CableRouter
 	//string grouping(string datastr);
 	//string inter_routing(string datastr);
 
-	class THMODULE_EXPORT CableRouteEngine
+	class CableRouteEngine
 	{
 	public:
 		string routing(string datastr);
